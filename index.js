7f5e9fa72ca9c5bf0a7065876cee2b84ac6e0070
merurend <merurw@gmail.com>
2019-01-14 11:21:21 +0700

    tambahin fitur delete & put

--- index.js
+++ index.js
@@ -41,5 +41,41 @@ app.post("/adduser", (req, res) => {
     })
 })
 
+app.delete("/deleteuser/:nama", (req, res) => {
+    MongoClient.connect(url, (err,db) => {
+        //mengambil koleksion table users ditaruh di usercol
+        userCol = db.collection("user");
+        //terima 2 parameter objek dari properti tabel nama dan funct
+        userCol.deleteOne({ nama: req.params.nama}, (err1, result) => {
+            db.close();
+            console.log(result);
+            res.send(result);
+        } )
+    })
+})
+
+app.put("/updateuser/:nama", (req,res) => {
+    MongoClient.connect(url, (err,db) => {
+        userCol = db.collection("user");
+        //
+        userCol.updateMany({ nama: req.params.nama}, {$set: req.body}, (err1, result) => {
+            db.close();
+            res.send(result);
+        })
+    })
+})
+
+app.put("/tambahketurunan", (req,res) => {
+    MongoClient.connect(url, (err,db) => {
+        userCol = db.collection("user");
+        //yg sebelah kiri adalah utk where, tengah perintah utk mengganti/yg mau di set
+        userCol.updateMany({}, {$set: {keturunan: "arab"}}, (err1, result) => {
+            db.close();
+            res.send(result);
+        })
+    })
+})
+
+
 
 app.listen(port, () => console.log("API aktif di port" + port));
